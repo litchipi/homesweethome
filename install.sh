fa306182d82231dd429f85bde1fe427cb9ce19ab
litchipi <litchi.pi@protonmail.com>
2020-04-29 19:07:59 +0200

    Adding automatic plugin installation with theme

--- install.sh
+++ install.sh
@@ -3,7 +3,14 @@
 sudo apt install python3-pip direnv tmux
 pip3 install base58 termenu
 
-sudo cp -r ./copydirs/tmux/ ~/.tmux
+cd ./copydirs/
+    cd ./tmux/
+        mkdir -p plugins && cd ./plugins
+        git clone https://github.com/jimeh/tmux-themepack
+        cp ../custom.tmuxtheme ./tmux-themepack
+    cd .. && mv ./plugins/ ~/.tmux/
+cd ..
+
 echo "############# HOME SWEET HOME #################" >> ~/.bashrc
 echo "eval '$(direnv hook bash)'">>~/.bashrc
 echo "################## END ########################" >> ~/.bashrc
