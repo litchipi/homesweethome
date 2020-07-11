0b43b6f4a5571ad94b4c9d78539144c055253e59
eve <eve@kali.pizza.home>
2020-07-11 20:14:16 +0200

    Improving TUI, building App bindings and commands for project management

--- src/core.py
+++ src/core.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python3
 #-*-encoding:utf-8*-
 
+import os
+
 from menu_handler import MenuHandler
 
-class Core:
+from cantreadth1s import CantReadThis
+
+class Core(MenuHandler):
     default_config = {}
 
     def __init__(self, config):
         self.config = dict.copy(self.default_config)
         self.config.update(config)
+        MenuHandler.__init__(self, self.config)
 
-        self.menu = MenuHandler(self.config)
+        self.crt = CantReadThis(sec_level=self.toolcfg["crt_seclevel"])
 
     def run(self):
-        self.menu.run()
+        super().run()
+        print(self.final_project)
+
+    def dispatch_menu_actions(self, action, *args, **kwargs):
+        if action == "backup":
+            project_path, pwd, project_name = args
+            self.crt.setup_preset_pwd(pwd)
+            self.crt.handle_directory(project_path, out=os.path.join(self.toolcfg["backup_directory"], project_name))
+
+        elif action == "restore":
+            project_name, pwd, restore_path = args
+            self.crt.setup_preset_pwd(pwd)
+            curd = os.path.abspath(os.curdir)
+            os.chdir(restore_path)
+            self.crt.handle_file(os.path.join(self.toolcfg["backup_directory"], project_name))
+            os.chdir(curd)
 
 def launch_hsh(args):
     core = Core(args.__dict__)
--- src/menu_handler.py
+++ src/menu_handler.py
@@ -12,14 +12,27 @@ class MenuHandler(nps.NPSAppManaged):
         self.test_var = "test var"
 
         self.project_selected = "toto"
+        self.projects_configs = dict()
+        self.final_project = None
         nps.NPSAppManaged.__init__(self)
 
     def onStart(self):
         self.addForm("select_project", MenuForm, name="Home Sweet Home")
-        self.addForm("start_project", StartProj, name="Start project")
+        self.addForm("start_project", StartProj, name="Start project", title="test title")
+
+    def start_project(self):
+        self.switchForm(None)
+        self.final_project = (self.project_selected, self.projects_configs[self.project_selected])
+
+    def get_project_configs(self, project_name):
+        # TODO      Grab configuration from stored file of the project tmuxp freeze
+        cfgs = {"test":"a", "test2":5, "test3":["abcdef", "akzel", "paopzeraz"]}
+        if project_name not in self.projects_configs.keys():
+            self.projects_configs[project_name] = cfgs
+        return cfgs
 
 # https://npyscreen.readthedocs.io/form-objects.html
-class MenuForm(nps.Form):
+class MenuForm(nps.FormBaseNew):
     def __init__(self, *args, **kwargs):
         self.ml_values = ["A", "B", "C"]
         self.curr_project = None
@@ -30,14 +43,16 @@ class MenuForm(nps.Form):
         self.project_choose = self.add(ProjectSelector, name="selection_project", values=self.ml_values, height=(len(self.ml_values)+1))
         self.text = self.add(nps.Textfield, value="")
         self.projectinfo = self.add(ProjectInfo, name="project_information")
+        self.projectinfo.get_info(self.ml_values[0])
 
 class ProjectInfo(nps.FixedText):
     def create(self):
         self.value = "Not implemented yet"
 
     def get_info(self, project):
-        self.value = "Project: " + project + " -> Not implemented yet"
-        self.value += "\n" + self.parent.parentApp.test_var
+        cfgs = self.parent.parentApp.get_project_configs(project)
+        self.value = "Project: " + project
+        self.value += "\n" + ", ".join(cfgs.keys())
         self.display()
 
 class ProjectSelector(nps.MultiLineAction):
@@ -48,17 +63,30 @@ class ProjectSelector(nps.MultiLineAction):
     def when_cursor_moved(self, *args, **kwargs):
         self.parent.projectinfo.get_info(self.values[self.cursor_line])
 
-class StartProj(nps.ActionForm):
+class StartProj(nps.FormBaseNew):
     def create(self):
-        self.text = self.add(nps.FixedText, value="tutu", invert_highlight_color=True)
+        self.start_butt = self.add(create_button(self.button_dispatcher, "start"), name="Launch Project")
+        self.start_butt = self.add(create_button(self.button_dispatcher, "abort"), name="Exit")
+        self.text = self.add(nps.FixedText, value=self.get_text(), editable=False)
         self.text.set_editable(False)
 
-    def while_editing(self, widg):
-        self.text.value="The selected project is: " + str(self.parentApp.project_selected)
- 
-    def on_ok(self):
-        sel = self.parentApp.project_selected
-        self.parentApp.switchForm(None)
+    def button_dispatcher(self, ident):
+        if ident == "start":
+            self.parentApp.start_project()
+        elif ident == "abort":
+            self.parentApp.switchForm(None)
 
-    def on_cancel(self):
-        self.parentApp.switchForm(None)
+    def get_text(self):
+        return "The selected project is: " + str(self.parentApp.project_selected)
+
+    def while_editing(self, widg):
+        self.text.value = self.get_text()
+        self.text.display()
+
+def create_button(callback, ident):
+    class CustomButton(nps.ButtonPress):
+        CUST_BUTTON_ID = ident
+        CUST_CUTTON_CALLBACK = callback
+        def whenPressed(self):
+            self.CUST_CUTTON_CALLBACK(self.CUST_BUTTON_ID)
+    return CustomButton
